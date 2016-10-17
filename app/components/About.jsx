2206e804dd1387215005b346c19251e3f033d4a2
Piotr Roczniak <proczniak@gmail.com>
2016-10-17 16:48:01 +0200

    Style to About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,49 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to About page.</p>
+      <p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - JS framework
+              used for the course.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">OpenWwather</a> - bubububu
+          </li>
+        </ul>
+      </p>
+        <table>
+    <thead>
+      <tr>
+        <th width="200">Table Header</th>
+        <th>Table Header</th>
+        <th width="150">Table Header</th>
+        <th width="150">Table Header</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Content Goes Here</td>
+        <td>This is longer content Donec id elit non mi porta gravida at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+      </tr>
+      <tr>
+        <td>Content Goes Here</td>
+        <td>This gravida at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+      </tr>
+      <tr>
+        <td>Content Goes Here</td>
+        <td>This is longer Content at eget metus.</td>
+        <td>Content Goes Here</td>
+        <td>Content Goes Here</td>
+      </tr>
+    </tbody>
+  </table>
     </div>
   )
 }
