7dd0d02cf8732561cc8d35c1e00602a6fffee13c
Piotr Roczniak <proczniak@gmail.com>
2016-10-17 14:00:45 +0200

    fixed typos

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,8 +10,11 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About</h3>
-    <p>Welcome to About page.</p>
+    <div>
+      <h3>About</h3>
+      <p>Welcome to About page.</p>
+    </div>
   )
 }
+
 module.exports = About;
